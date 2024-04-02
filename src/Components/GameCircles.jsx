f26f578a6e478c0686b8be0576ae680550523ef3
Gareth Dean <gazdean@hotmail.com>
2024-04-02 11:45:47 +0100

    play screen touch functionality added

--- src/Components/GameCircles.jsx
+++ src/Components/GameCircles.jsx
@@ -3,8 +3,8 @@ import SquircleContainer from "./SquircleContainer";
 
 export default function GameCircles({ gameDiameters, setTouching}) {
     return (
-        <SquircleContainer  onTouchStart={()=>setTouching(true)}
-        onTouchEnd={()=>setTouching(false)}>
+        <SquircleContainer  ontouchstart={()=>setTouching(true)}
+        ontouchend={()=>setTouching(false)}>
             <IndividualSquircle diameter={gameDiameters.green} color={"green"} borderType={"solid"}/>      
             <IndividualSquircle diameter={gameDiameters.red} color={"red"} borderType={"solid"}/>      
             <IndividualSquircle diameter={gameDiameters.blue} color={"blue"} borderType={"solid"}/>
